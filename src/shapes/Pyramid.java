1b8c316d57ab7e122a7d44e565ae02c4593a4c21
kkhan4724 <152225776+kkhan4724@users.noreply.github.com>
2024-10-10 11:45:37 -0600

    Update Pyramid.java
    Added Pyramid class

--- src/shapes/Pyramid.java
+++ src/shapes/Pyramid.java
@@ -1,6 +1,37 @@
 package shapes;
 
-public class Pyramid
-{
 
+public class Pyramid extends Shape {
+    private double height;
+    private double edgeLength;
+
+    // Constructor
+    public Pyramid(double height, double edgeLength) {
+        this.height = height;
+        this.edgeLength = edgeLength;
+    }
+
+    // Calculates the base area of the pyramid (assuming a square base)
+    @Override
+    public double calcBaseArea() {
+        return Math.pow(edgeLength, 2);
+    }
+
+    // Calculates the volume of the pyramid: (1/3) * base area * height
+    @Override
+    public double calcVolume() {
+        return (1.0 / 3.0) * calcBaseArea() * height;
+    }
+
+    // Returns the height of the pyramid
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Pyramid [Height=" + height + ", Edge Length=" + edgeLength + "]";
+    }
 }
