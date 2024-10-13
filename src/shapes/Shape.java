9f51d756a80e6294a787713ee77b852db1383e37
J-Wales <j.wales@edu.sait.ca>
2024-10-13 10:15:23 -0600

    Fixed implementations of Shape

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -1,37 +1,5 @@
-package utilities;
+package shapes;
 
-import java.util.Comparator;
+public abstract class Shape {
 
-public abstract class Shape implements Comparable<Shape>, Comparator<Shape>{
-
-	public abstract double getHeight();
-	public abstract double getVolume();
-	public abstract double getArea();
-	
-	@Override
-	public int compareTo(Shape other) {
-		if (getHeight() > other.getHeight()) {
-			return -1;
-		}
-		
-		if (getHeight() == other.getHeight()) {
-			return 0;
-		}
-		
-		return 1;
-	}
-	
-	public static Comparator<Shape> AreaComparator = new Comparator<Shape>() {
-        @Override
-        public int compare(Shape s1, Shape s2) {
-            return Double.compare(s1.getArea(), s2.getArea());
-        }
-	};
-	
-	public static Comparator<Shape> VolumeComparator = new Comparator<Shape>() {
-        @Override
-        public int compare(Shape s1, Shape s2) {
-            return Double.compare(s1.getVolume(), s2.getVolume());
-        }
-	};
 }
