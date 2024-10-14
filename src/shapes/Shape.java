729d0d3f67e0e9edcfbee8672a57474964db147a
kkhan4724 <152225776+kkhan4724@users.noreply.github.com>
2024-10-14 13:43:02 -0600

    Update Shape.java

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -2,35 +2,33 @@ package shapes;
 
 import java.util.Comparator;
 
-public abstract class Shape implements Comparable<Shape>, Comparator<Shape>{
+public abstract class Shape implements Comparable<Shape> {
 
-	public abstract double getHeight();
-	public abstract double getVolume();
-	public abstract double getArea();
-	
-	@Override public int compareTo(Shape other) {
-		if (getHeight() > other.getHeight()) {
-			return -1;
-		}
-		
-		if (getHeight() == other.getHeight()) {
-			return 0;
-		}
-		
-		return 1;
-	}
-	
-	public static Comparator<Shape> AreaComparator = new Comparator<Shape>() {
+    // Abstract methods for height, volume, and area
+    public abstract double getHeight();
+    public abstract double getVolume();
+    public abstract double getArea();
+
+    // Implements the compareTo method to compare by height in descending order
+    @Override
+    public int compareTo(Shape other) {
+        // Compare heights in descending order
+        return Double.compare(other.getHeight(), this.getHeight());
+    }
+
+    // Comparator to compare shapes by base area in ascending order
+    public static Comparator<Shape> AreaComparator = new Comparator<Shape>() {
         @Override
         public int compare(Shape s1, Shape s2) {
             return Double.compare(s1.getArea(), s2.getArea());
         }
-	};
-	
-	public static Comparator<Shape> VolumeComparator = new Comparator<Shape>() {
+    };
+
+    // Comparator to compare shapes by volume in ascending order
+    public static Comparator<Shape> VolumeComparator = new Comparator<Shape>() {
         @Override
         public int compare(Shape s1, Shape s2) {
             return Double.compare(s1.getVolume(), s2.getVolume());
         }
-	};
+    };
 }
