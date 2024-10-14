f88d22921af76aecd98f0ddb2a7b428be4cca49a
J-Wales <j.wales@edu.sait.ca>
2024-10-14 13:34:11 -0600

    Push that github is telling me I have to do

--- src/shapes/Cylinder.java
+++ src/shapes/Cylinder.java
@@ -1,6 +1,37 @@
 package shapes;
 
-public class Cylinder
-{
 
+
+public class Cylinder extends Shape
+{
+	public double height;
+	
+	public double radius;
+	
+	public double baseArea;
+	
+	public double volume;
+	
+	public Cylinder(double height, double radius) {
+		this.height = height;
+		this.radius = radius;
+		
+		baseArea = (Math.PI * Math.pow(radius, 2));
+		volume = (Math.PI * Math.pow(radius, 2) * height);
+	}
+	
+	@Override
+	public double getHeight() {
+		return this.height;
+	}
+	
+	@Override
+	public double getVolume() {
+		return this.volume;
+	}
+	
+	@Override
+	public double getArea() {
+		return this.baseArea;
+	}
 }
