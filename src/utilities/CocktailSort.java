2f91a635ff7246999c635cfb34288c9f00032235
kkhan4724 <152225776+kkhan4724@users.noreply.github.com>
2024-10-17 13:33:18 -0600

    Update CocktailSort.java

--- src/utilities/CocktailSort.java
+++ src/utilities/CocktailSort.java
@@ -1,5 +1,39 @@
 package utilities;
+import java.util.Comparator;
 
 public class CocktailSort {
+   // Generic method to perform cocktail sort
+   public static <T> void cocktailSort(T[] arr, Comparator<? super T> comparator) {
+       boolean swapped = true;
+       int start = 0;
+       int end = arr.length;
+       while (swapped) {
+           swapped = false;
 
+           for (int i = start; i < end - 1; i++) {
+               if (comparator.compare(arr[i], arr[i + 1]) > 0) {
+                   T temp = arr[i];
+                   arr[i] = arr[i + 1];
+                   arr[i + 1] = temp;
+                   swapped = true;
+               }
+           }
+
+           if (!swapped) break;
+
+           swapped = false;
+           end--;
+
+           for (int i = end - 1; i >= start; i--) {
+               if (comparator.compare(arr[i], arr[i + 1]) > 0) {
+                   T temp = arr[i];
+                   arr[i] = arr[i + 1];
+                   arr[i + 1] = temp;
+                   swapped = true;
+               }
+           }
+
+           start++;
+       }
+   }
 }
